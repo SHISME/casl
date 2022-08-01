7c2e56cc44a9c4be0f04020b073714f2c5fb8ac2
SuHai <hai.su@shopee.com>
2022-08-01 11:27:01 +0800

    fix: 移除无关项目

--- packages/casl-prisma/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { prismaQuery } from './prisma/PrismaQuery';
-export type { PrismaQuery, Model, Subjects } from './prisma/PrismaQuery';
-export { accessibleBy } from './accessibleBy';
-export { PrismaAbility } from './PrismaAbility';
-export { ParsingQueryError } from './errors/ParsingQueryError';
